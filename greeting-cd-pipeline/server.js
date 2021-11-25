438818c2c3ba0e84370dd23c3eee4f0193927e74
Student User <student@workstation.lab.example.com>
2021-11-25 01:53:48 -0500

    error

--- greeting-cd-pipeline/server.js
+++ greeting-cd-pipeline/server.js
@@ -4,10 +4,10 @@ const greet = require("./greet");
 
 const server = http.createServer((req, res) => {
     const { name } = url.parse(req.url, true).query;
-    res.statusCode = 200;
+    res.statusCode = 500;
     res.setHeader("Content-Type", "text/plain");
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.end(greet(name));
+    res.end(greet("Error"));
 });
 
 const port = 3000;
